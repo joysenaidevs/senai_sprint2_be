bee30a6e33f510bdd62aac3031efb9b9cbfadc73
Joyce <joycessilva17@hotmail.com>
2021-03-18 05:56:27 -0300

    Repositorio de FilmesRepository adicionado

--- Sprint 2 - Beck End/senai_filmes_webApi/senai_filmes_webApi/Interfaces/IFilmesRepository.cs
+++ Sprint 2 - Beck End/senai_filmes_webApi/senai_filmes_webApi/Interfaces/IFilmesRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using senai_filmes_webApi.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,27 @@ namespace senai_filmes_webApi.Interfaces
 {
     public interface IFilmesRepository
     {
+
+        //  tipo de retorno do metódo, nome do metedo, invocando uma funçao: ()
+        //          tipoRetorno           NomeMetodo         (tipoParametro nomeParametro);
+        //      void Cadastrar(); -- sem retorno e não recebe parâmetro
+        // fim exemplo
+
+
+        // Este metodo retorna todos os Filmes
+        List<FilmesDomain> ListarTodos();
+
+
+        // Busca um filme através do seu id
+        FilmesDomain BuscarPorId(int id);
+
+        // Cadastra um novo filme
+        void Cadastrar(FilmesDomain novoFilme);
+
+        // Atualiza um filme existente passando o id pelo corpo da requisição
+        void AtualizarIdCorpo(FilmesDomain titulo);
+
+        // deleta um filme , passando como parametro o id do filme que será deletado
+        void Deletar(int id);
     }
 }

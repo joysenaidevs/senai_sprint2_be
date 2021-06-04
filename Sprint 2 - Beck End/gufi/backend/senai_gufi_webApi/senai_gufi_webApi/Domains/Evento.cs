669906066bff66fb0e938704fbfaa87a7c82279d
Joyce Silva <joycessilva17@hotmail.com>
2021-06-04 00:27:41 -0300

    API ADICIONADA

---
+++ Sprint 2 - Beck End/gufi/backend/senai_gufi_webApi/senai_gufi_webApi/Domains/Evento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai_gufi_webApi.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade (tabela) de eventos
+    /// </summary>
+    public partial class Evento
+    {
+        public Evento()
+        {
+            Presencas = new HashSet<Presenca>();
+        }
+
+        public int IdEvento { get; set; }
+        public int? IdTipoEvento { get; set; }
+        public int? IdInstituicao { get; set; }
+        public string NomeEvento { get; set; }
+        public bool? AcessoLivre { get; set; }
+        public DateTime DataEvento { get; set; }
+        public string Descricao { get; set; }
+
+        public virtual Instituico IdInstituicaoNavigation { get; set; }
+        public virtual TiposEvento IdTipoEventoNavigation { get; set; }
+        public virtual ICollection<Presenca> Presencas { get; set; }
+    }
+}
---
+++ Sprint 2 - Beck End/gufi/backend/senai_gufi_webApi/senai_gufi_webApi/Domains/Instituico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai_gufi_webApi.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade (tabela) de instituições
+    /// </summary>
+    public partial class Instituico
+    {
+        public Instituico()
+        {
+            Eventos = new HashSet<Evento>();
+        }
+
+        public int IdInstituicao { get; set; }
+        public string Cnpj { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Endereco { get; set; }
+
+        public virtual ICollection<Evento> Eventos { get; set; }
+    }
+}
